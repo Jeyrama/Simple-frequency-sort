6ff338816b693aadedabdad5d1edf4adc89d40f6
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-26 09:55:15 -0800

    Update Simple-frequency-sort.js

--- Simple-frequency-sort.js
+++ Simple-frequency-sort.js
@@ -8,3 +8,6 @@ solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
 -- We sort by highest frequency to lowest frequency.
 -- If two elements have same frequency, we sort by increasing value.
 */
+
+
+// Solution
