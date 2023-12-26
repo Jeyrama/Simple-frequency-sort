6b3a7c8d02fc4ef82651b875c2ec4b60d8e015d2
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-26 10:12:12 -0800

    Update Simple-frequency-sort.js

--- Simple-frequency-sort.js
+++ Simple-frequency-sort.js
@@ -13,6 +13,21 @@ solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
 // Solution
 
 function solve(arr) {
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (obj.hasOwnProperty(arr[i])) {
+      obj[arr[i]]++;
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
+  arr = arr.sort((a,b) => obj[b] === obj[a] ? a-b : obj[b]-obj[a]);
+  return arr;
+}
+
+// or
+
+function solve(arr) {
   let r = {}
   for (let n of arr) r[n]=r[n]+1 || 1
   return arr.slice().sort((a,b) => r[b] - r[a] || a-b)
